92faa9f272fdf78e80ed6964f9d97ea83b647817
Sivasanath06 <100675167+Sivasanath06@users.noreply.github.com>
2022-12-29 17:42:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = process.env.URL_API;
 root.render(
   <React.StrictMode>
     <App />
